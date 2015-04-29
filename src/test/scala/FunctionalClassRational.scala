e6db6e95752f2a9cb7d975b66cab9f5e2aea92ac
Ben Wu <wubin28@gmail.com>
2015-04-29 09:24:27 +0800

    Finished test 'the toString method is overridden correctly'.

--- src/test/scala/FunctionalClassRational.scala
+++ src/test/scala/FunctionalClassRational.scala
@@ -4,9 +4,13 @@ import org.scalatest.FunSuite
  * Created by twer on 4/29/15.
  */
 class FunctionalClassRational extends FunSuite{
-  test("the toString method is overridden correctly")
+  test("the toString method is overridden correctly") {
+    var oneThird = new Rational(1, 3)
 
-  var oneThird = new Rational(1, 3)
+    assert("1/3" === oneThird.toString)
+  }
+}
 
-  assert("1/3" === oneThird)
+class Rational(n: Int, d: Int) {
+  override def toString = n + "/" + d
 }
