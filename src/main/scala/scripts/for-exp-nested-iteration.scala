de8d868d4ad7fcb5dbd0ab22b6b5b17019b5b65f
Ben Wu <wubin28@gmail.com>
2015-05-02 12:45:45 +0800

    Added script for-exp-nested-iteration.scala with Nested iteration.

--- src/main/scala/scripts/for-exp-nested-iteration.scala
+++ src/main/scala/scripts/for-exp-nested-iteration.scala
@@ -34,3 +34,17 @@ for (
   if file.isDirectory
   if file.getName.endsWith("Folder")
 ) println(file)
+
+// Nested iteration
+println("\n>>>for if file name ends with .scala and line contains endsWith:")
+def fileLines(file: java.io.File) =
+  scala.io.Source.fromFile(file).getLines().toList
+def grep(pattern: String) =
+  for (
+    file <- filesHere
+    if file.getName.endsWith(".scala");
+    line <- fileLines(file)
+    if line.trim.matches(pattern)
+  ) println(file +": "+ line.trim)
+grep(".*endsWith.*")
+
