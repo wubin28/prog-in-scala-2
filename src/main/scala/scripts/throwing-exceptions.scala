4bec0ea423ed547fb7aa665d474927a983ee879d
Ben Wu <wubin28@gmail.com>
2015-05-03 20:17:21 +0800

    Added finally clause in the script.

--- src/main/scala/scripts/throwing-exceptions.scala
+++ src/main/scala/scripts/throwing-exceptions.scala
@@ -13,12 +13,22 @@ import java.io.FileReader
 import java.io.FileNotFoundException
 import java.io.IOException
 
-val filename = "list.ss"
+val filename = "list1.ss"
 try {
-  val f = new FileReader(filename)
-  // Use and close file
-  println("found file: " + filename)
+  val file = new FileReader(filename)
+  try {
+    // Use the file
+  } finally {
+    file.close()  // Be sure to close the file
+    println("closed file: " + filename)
+  }
 } catch {
+  // The behavior of this try-catch expression is the same as in other languages with
+  // exceptions. The body is executed, and if it throws an exception, each catch
+  // clause is tried in turn. In this example, if the exception is of type
+  // FileNotFoundException, the first clause will execute. If it is of type
+  // IOException, the second clause will execute. If the exception is of neither type,
+  // the try-catch will terminate and the exception will propagate further.
   case ex: FileNotFoundException => // Handle missing file
     println("File '" + filename + "' not found")
   case ex: IOException => // Handle other I/O error
